ce25abb2a891542d60425fdafec2bdb4a598bd28
Marcin <marcinszydelko@yahoo.com>
2020-01-28 16:05:12 +0000

    add player to each song

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const SongItem = ({song}) => {
     return (
-    <li>
-    <b>{song["im:name"].label}</b> - 
-    {song["im:artist"].label}
-    </li>
+        <li>
+        <b>{song["im:name"].label}</b> - 
+        {song["im:artist"].label}
+        <br/>
+        <audio controls>
+            <source src={song.link[1].attributes.href} type="audio/x-m4a"></source>
+        </audio>
+        </li>
     )
 }
 
